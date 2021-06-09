5593bf941a26eecb95906aed3ff594aa4fe725a6
Georgia Kanli <kanli.g@gmail.com>
2021-06-09 16:26:33 +0200

    Add predict file
    Building the Command Line Application
    
    # Correct a display error in the jupyter

--- DeepLearning/Project/predict.py
+++ DeepLearning/Project/predict.py
@@ -4,32 +4,28 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import tensorflow_hub as hub
+tfds.disable_progress_bar()
 
 #  Ingore error TensorFlow
 import warnings
 warnings.filterwarnings('ignore')
 
-%matplotlib inline
-%config InlineBackend.figure_format = 'retina'
-
 import time
 import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 import json
-import tensorflow_datasets as tfds
-tfds.disable_progress_bar()
 import os
+import tkinter as tk
+from PIL import Image
+from pathlib import Path
+from tkinter import messagebox    
+import sys
 
 import logging
 logger = tf.get_logger()
 logger.setLevel(logging.ERROR)
-
-
-import warnings
-warnings.filterwarnings('ignore')
-%matplotlib inline
-%config InlineBackend.figure_format = 'retina'
-from PIL import Image
 
 
 # Load oxford_flowers102 dataset
@@ -40,6 +36,81 @@ def load_dataset():
     training_set_f, validation_set_f, test_set_f = dataset_f['train'], dataset_f['validation'], dataset_f['test']
     return training_set_f, validation_set_f, test_set_f, dataset_f, dataset_info_f
 
+def duild_model(num_classes):
+    # Load MobileNet pre-trained network (feature_vector, without classification layer as we will add our own)
+    URL = "https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/4"
+    feature_extractor = hub.KerasLayer(URL, input_shape=(224, 224, 3))
+    feature_extractor.trainable = False
+    model = tf.keras.Sequential([feature_extractor,
+                                 tf.keras.layers.Dense(num_classes, activation='softmax')])
+    # Stop training when there is no improvement in the validation loss for 5 consecutive epochs
+    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
+    # Compile the model with optimizer, loss and metrics parameters
+    model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+    model.summary()
+    return model
+
+
+# Train the model and validate
+def train_model(model, EPOCHS):
+    train_history = model.fit(training_batches,
+                          epochs=EPOCHS,
+                          validation_data=validation_batches)
+    return model, train_history
+
+
+# Plot the loss and accuracy values achieved during training for the training and validation set.   
+def plot_progress(train_history):
+    training_accuracy = train_history.history['accuracy']
+    validation_accuracy = train_history.history['val_accuracy']
+    training_loss = train_history.history['loss']
+    validation_loss = train_history.history['val_loss']
+    epochs_range = range(len(training_accuracy))
+    plt.figure(figsize=(8, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, training_accuracy, label='Training Accuracy')
+    plt.plot(epochs_range, validation_accuracy, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, training_loss, label='Training Loss')
+    plt.plot(epochs_range, validation_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.show()    
+    plt.savefig('./prediction_result/valos.png')
+
+
+# Print the loss and accuracy values achieved on the entire test set.
+def evaluate(model):
+    loss, accuracy = model.evaluate(test_batches)
+    return loss, accuracy
+
+# Check some predictions to see visually how the model is performing
+def plot_prediction(test_batches):
+    for image_batch, label_batch in test_batches.take(1):
+        ps = model.predict(image_batch)
+        images = image_batch.numpy().squeeze()
+        labels = label_batch.numpy()
+
+    plt.figure(figsize=(10,15))
+    for n in range(30):
+        plt.subplot(6,5,n+1)
+        plt.imshow(images[n], cmap = plt.cm.binary)
+        color = 'green' if np.argmax(ps[n]) == labels[n] else 'red'
+        plt.title(class_names[str(np.argmax(ps[n])+1)], color=color)
+        plt.axis('off')        
+    plt.savefig('./prediction_result/predict_test_set.png')
+        
+        
+# Save the trained model as a Keras model.
+def save_model(model):
+    saved_path = './{}.h5'.format('saved_model_1')
+    model.save(saved_path)
+    
+    
 # Normalize
 def normalize_image(image, label):
     image = tf.cast(image, tf.float32)
@@ -77,11 +148,11 @@ def process_test_image():
 
 
 # Create the predict function
-def predict(image_path=None, model=None, top_k=None):
+def predict(image_path, model, top_k):
     im = Image.open(image_path)
     test_image = np.asarray(im)
     processed_test_image = process_image(test_image)
-    processed_test_image=np.expand_dims(processed_test_image,0)
+    processed_test_image = np.expand_dims(processed_test_image,0)
     probs = model.predict(processed_test_image)
     return tf.nn.top_k(probs, k=top_k)
 
@@ -97,42 +168,88 @@ def prediction_result(filename):
 
     im = Image.open(filename)
     test_image = np.asarray(im)
-    axis_label=filtered(classes)
-    fig,(ax1,ax2)=plt.subplots(figsize=(10,3),ncols=2)
+    axis_label = filtered(classes+1)
+    fig,(ax1,ax2) = plt.subplots(figsize=(10,3),ncols=2)
     ax1.imshow(test_image,cmap=plt.cm.binary)
-    ax2.set_title('Class Probability')
+    ax2.set_title(str('Class Probability - ' + Path(filename).stem))
     ax2.barh(np.array(axis_label),probs[0])
+    save_prediction_result = str('./prediction_result/'+ Path(filename).stem + '.png' )
+    plt.savefig(save_prediction_result)
+
+
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+            It must be "yes" (the default), "no" or None (meaning
+            an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == "":
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")
 
 # Main function
 if __name__ == '__main__':
-    # Dataset
-    training_set, validation_set, test_set, dataset, dataset_info = load_dataset()
 
     # Hyper parameters
     batch_size = 34
     image_size = (224, 224)
-    total_training_samples = num_validation = dataset_info.splits['validation'].num_examples
-
-
-    # Create the training, validation and test batches
-    training_batches = training_set.shuffle(total_training_samples // 2).map(normalize_image).batch(
-        batch_size).prefetch(1)
-    validation_batches = validation_set.map(normalize_image).batch(batch_size).prefetch(1)
-    test_batches = test_set.map(normalize_image).batch(batch_size).prefetch(1)
+    EPOCHS = 15
 
     # Load the class names
     with open('label_map.json', 'r') as f:
         class_names = json.load(f)
-
-    # Load the model
-    model = tf.keras.models.load_model(str('./' + 'saved_model'), custom_objects={'KerasLayer': hub.KerasLayer})
-
-    # Load the model
+    
+    ans = query_yes_no("Build and Train the Classifier? ")
+    
+    if ans == 0:
+        # Load the model
+        saved_path = './{}.h5'.format('saved_model')
+        model = tf.keras.models.load_model(saved_path, custom_objects={'KerasLayer': hub.KerasLayer})
+    else:
+        training_set, validation_set, test_set, dataset, dataset_info = load_dataset()
+        # Create the training, validation and test batches
+        total_training_samples = dataset_info.splits['validation'].num_examples
+        training_batches = training_set.shuffle(total_training_samples//2).map(normalize_image).batch(batch_size).prefetch(1)
+        validation_batches = validation_set.map(normalize_image).batch(batch_size).prefetch(1)
+        test_batches = test_set.map(normalize_image).batch(batch_size).prefetch(1)
+        # Build your network.
+        num_classes = dataset_info.features['label'].num_classes
+        model = duild_model(num_classes)
+        # Train the model and validate
+        model, train_history = train_model(model,EPOCHS)
+        # Check some predictions to see visually how the model is performing
+        plot_progress(train_history)
+        # Print the loss and accuracy values achieved on the entire test set.
+        loss, accuracy = evaluate(model)
+        # Check some predictions to see visually how the model is performing
+        plot_prediction(test_batches)
+        # Save the trained model as a Keras model.
+        save_model(model)    
+    
+    # Prediction 
     folder = './test_images/'
     filenames = load_images_path(folder)
 
     for image_path in filenames:
         prediction_result(image_path)
-
-
 
